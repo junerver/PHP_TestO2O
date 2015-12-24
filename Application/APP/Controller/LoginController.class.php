e609b424c5dbf1168b6708020709764514d6eab4
junerver <junerver@gmail.com>
2015-12-24 15:35:49 +0800

    优化了代码

--- Application/APP/Controller/LoginController.class.php
+++ Application/APP/Controller/LoginController.class.php
@@ -13,27 +13,27 @@ use Think\Controller;
 class LoginController extends Controller
 {
     //验证码方法
-    Public function verify()
-    {
-        $config = array(
+	Public function verify()
+	{
+		$config = array(
             'fontSize' => 30,    // 验证码字体大小
             'length' => 4,     // 验证码位数
             'useNoise' => false, // 关闭验证码杂点
 //            'imageW'=>150,
 //            'imageH'=>40
-        );
-        $Verify = new \Think\Verify($config);
-        $Verify->entry();
+            );
+		$Verify = new \Think\Verify($config);
+		$Verify->entry();
 
 
-    }
+	}
 
-    public function login()
-    {
+	public function login()
+	{
 
-        $this->assign('title', '登陆');
-        $userModel = D('User');
-        if (IS_POST) {
+		$this->assign('title', '登陆');
+		$userModel = D('User');
+		if (IS_POST) {
 
             $this->assign('old', I('post.'));  //保存上次请求
             $code = I('post.verifycode');   //从请求中获取验证码
@@ -41,34 +41,32 @@ class LoginController extends Controller
             $username = I('post.username');
             $password = md5(I('post.password'));
             $result = $userModel->where(array('username' => $username))->find();
+            //链式判断，使用elseif
             if ($result == null || empty($result)) {
                 //用户不存在
                 $error['username'] = '用户名不存在！';
                 $this->assign('errors', $error);
                 $this->display();
-            } else {
-                if ($result['password'] != $password) {
-                    //密码不匹配
-                    $error['password'] = '密码错误！';
-                    $this->assign('errors', $error);
-                    $this->display();
-                } else {
-                    if (!check_verify($code)) {
-                        $error['verify'] = '验证码错误！';
-                        $this->assign('errors', $error);
-                        $this->display();
-                    } else {
-                        //均匹配
-                        session('username', $username);
-                        $this->success('登陆成功！', U('Index/index'), 2);
-                    }
-
-                }
+            } elseif ($result['password'] != $password) {
+                //密码不匹配
+                $error['password'] = '密码错误！';
+                $this->assign('errors', $error);
+                $this->display();
+            }elseif (!check_verify($code)) {
+                $error['verify'] = '验证码错误！';
+                $this->assign('errors', $error);
+                $this->display();
+            }else {
+                //均匹配
+                session('username', $username);
+                $this->success('登陆成功！', U('Index/index'), 2);
             }
         } else {
             //无提交
-            $this->display();
+        	$this->display();
         }
+
+        
+
     }
 }
-
